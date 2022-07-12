dce6ad2f69140e114cad799ddb30b5f10c2ba14a
CamilaNunes <cvn.camila@gmail.com>
2022-07-12 20:39:31 -0300

    Criando e configurando inicio do projeto;

---
+++ virtualShop/virtualShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using virtualShop.ProductApi.Models;
+
+namespace virtualShop.ProductApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
