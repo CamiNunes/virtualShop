d0c24f048eac9a414fff2b07f0cb6644022e0823
CamilaNunes <cvn.camila@gmail.com>
2022-07-12 23:57:22 -0300

    criando migrations;

--- virtualShop/virtualShop.ProductApi/Context/AppDbContext.cs
+++ virtualShop/virtualShop.ProductApi/Context/AppDbContext.cs
@@ -12,5 +12,33 @@ namespace virtualShop.ProductApi.Context
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        //Fluente API
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Category>().HasKey(c => c.Id);
+            mb.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+            mb.Entity<Product>().HasKey(p => p.Id);
+            mb.Entity<Product>().Property(p => p.Name).HasMaxLength(100).IsRequired();
+            mb.Entity<Product>().Property(p => p.Description).HasMaxLength(255).IsRequired();
+            mb.Entity<Product>().Property(p => p.ImageURL).HasMaxLength(255).IsRequired();
+            mb.Entity<Product>().Property(p => p.Price).HasPrecision(12,2);
+
+            mb.Entity<Category>().HasMany(c => c.Products).WithOne(p => p.Category).IsRequired().OnDelete(DeleteBehavior.Cascade);
+
+            mb.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Material Escolar",
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Acessórios",
+                }
+            );
+        }
     }
 }
